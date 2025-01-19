c15d0d0c8b993692f4a73720b8a0ac8a4bad9d03
miky-rola <mikyrola8@gmail.com>
2025-01-19 12:21:10 +0000

    updated compressors

--- src/compressor.rs
+++ src/compressor.rs
@@ -220,25 +220,6 @@ impl Compressor {
         Ok(total_bytes)
     }
 
-    // fn collect_files(dir: &Path) -> io::Result<Vec<PathBuf>> {
-    //     let mut files = Vec::new();
-    //     if dir.is_file() {
-    //         files.push(dir.to_path_buf());
-    //     } else if dir.is_dir() {
-    //         for entry in fs::read_dir(dir)? {
-    //             let entry = entry?;
-    //             let path = entry.path();
-    //             if path.is_file() {
-    //                 files.push(path);
-    //             } else if path.is_dir() {
-    //                 files.extend(Self::collect_files(&path)?);
-    //             }
-    //         }
-    //     }
-    //     Ok(files)
-    // }
-
-
     pub fn decompress(&self) -> io::Result<()> {
         let input_file = File::open(&self.input_path)?;
         let reader = BufReader::new(input_file);
